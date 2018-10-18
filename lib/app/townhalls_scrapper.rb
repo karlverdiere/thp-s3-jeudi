a2aaed2e12089ece9a1fb6d8f4129eca5ce5673e
MacDesu <32233686+macdesu@users.noreply.github.com>
2018-10-18 15:30:59 +0200

    asdf

--- lib/app/townhalls_scrapper.rb
+++ lib/app/townhalls_scrapper.rb
@@ -7,7 +7,6 @@ require "csv"
 class Scrapper
 	public
 	def initialize(html_page, path, zipcode)
-		@email_list = Array.new
 		@html_page = html_page
 		@path = path
 		@zipcode = zip_to_regex(String(zipcode)) # Transforme le code en string dans tous les cas
@@ -26,11 +25,12 @@ class Scrapper
 
 	# Recupere les URL de chaque mairie et appelle get_email_webpage pour chacune d'entre elle
 	def get_townhalls_url
+		@email_list = Array.new
 		page = Nokogiri::HTML(open(@html_page))
 		puts(@html_page + "\nFetching emails...\n" + @html_page)
 		page.xpath("//@href").grep(@zipcode).each do |link|
 			link = link.to_s
-			puts @email_list.push(get_email_webpage(link.gsub("./", "http://annuaire-des-mairies.com/")))
+			@email_list.push(get_email_webpage(link.gsub("./", "http://annuaire-des-mairies.com/")).to_s)
 		end
 		puts "...DONE !"
 		return @email_list
@@ -40,9 +40,9 @@ class Scrapper
 	def get_email_webpage(url)
 		h = Hash.new
 		page = Nokogiri::HTML(open(url))
-		town_name = page.xpath("/html/body/div/main/section[1]/div/div/div/h1").text.split(" - ")
 		email = page.xpath(@path).text
-		return h = { [town_name[0]] => email }
+		town_name = page.xpath("/html/body/div/main/section[1]/div/div/div/h1").text.split(" - ")
+		return h = { [town_name[0].to_sym, town_name[1].to_sym] => email }
 	end
 end
 
@@ -50,7 +50,7 @@ def scrapping
 	xpath = "/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]"
 	scrapper = Scrapper.new("http://www.annuaire-des-mairies.com/martinique.html", xpath, 972)
 
-	File.open("db/emails.JSON", "w") do |f|
+	File.open("db/emails.JSON","w") do |f|
 		f.write(scrapper.start)
 	end
 end
